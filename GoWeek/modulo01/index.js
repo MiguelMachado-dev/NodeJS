f40b1ac98498c19349e94d0024c24700ce4ffd9e
Miguel Machado <miiguellmp@gmail.com>
2019-02-18 16:37:15 -0300

    configuração do nunjucks para index principal

--- GoWeek/modulo01/index.js
+++ GoWeek/modulo01/index.js
@@ -1,33 +1,19 @@
 //const varName = require('nome-da-biblioteca')
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
 
-// Middleware é tudo o que utiliza req, res
-const logMiddleware = (req, res, next) => {
-  console.log(
-    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
-  // colocar o parametro next no final do middleware e chamar
-  // ele no middleware em que não deve bloquear o fluxo do ExpressJS
-  return next();
-};
-
-// Podemos usar o Middleware depois de cada rota
-// Ou podemos colocar para toda nossa aplicação utilize o middleware
-app.use(logMiddleware);
-
-app.get("/", logMiddleware, (req, res) => {
-  return res.send(`Bem-vindo, ${req.query.name}`);
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
 });
 
-app.get("/login", (req, res) => {
-  return res.send("Login");
-});
+app.set("view engine", "njk");
 
-// :name = tudo depois de dois pontos vira parametro
-app.get("/nome/:name", (req, res) => {
-  return res.send(`Bem-vindo, ${req.params.name}`);
+app.get("/", (req, res) => {
+  return res.render("list", { name: "Miguel" });
 });
 
 app.listen(3000);
