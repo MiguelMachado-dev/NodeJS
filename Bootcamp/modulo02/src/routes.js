2e62b7e8b544155c9cea56d8d6ed368aaa8b8acb
Miguel Machado <miiguellmp@gmail.com>
2019-02-22 16:58:05 -0300

    setando rotas utilizarem middlewares de auth

--- Bootcamp/modulo02/src/routes.js
+++ Bootcamp/modulo02/src/routes.js
@@ -5,17 +5,26 @@ const SessionController = require('./app/controllers/SessionController')
 const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
+// importação de middleware que verifica se usuario já está logado
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 // rota chamada quando o usuario solicita o /
-routes.get('/', SessionController.create)
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 // avatar é o campo no banco de dados
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+// Todas as rotas que iniciam com app apliquem esse middleware
+// usuário só conseguirá acessar a rota /app/* quando estiver logado
+routes.use('/app', authMiddleware)
 
 // rota obtida quando o usuario solicita /app/dashboard
 // que retorna o template dashboard
 routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session)
   console.log(req.session.user)
   return res.render('dashboard')
 })
