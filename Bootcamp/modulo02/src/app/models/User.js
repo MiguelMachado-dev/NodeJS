d40f5b9d36448ee2a57933f538ded4d894d7b1c3
Miguel Machado <miiguellmp@gmail.com>
2019-02-22 09:37:14 -0300

    funções de quando usuario solicita a pagina de cadastro e salva infos no bd

---
+++ Bootcamp/modulo02/src/app/models/User.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs')
+
+module.exports = (sequelize, DataTypes) => {
+  // Cria uma table no banco de dados com nome User
+  const User = sequelize.define(
+    'User',
+    {
+      // Propriedades que cada User terá de informação
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
+      provider: DataTypes.BOOLEAN
+    },
+    {
+      hooks: {
+        beforeSave: async user => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8)
+          }
+        }
+      }
+    }
+  )
+
+  return User
+}
