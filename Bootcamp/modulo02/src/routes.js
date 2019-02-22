74d4f621e3d32e8a03c8c417b684b99a615184e7
Miguel Machado <miiguellmp@gmail.com>
2019-02-22 10:16:14 -0300

    instalação do multer para envio de foto para banco de dados

--- Bootcamp/modulo02/src/routes.js
+++ Bootcamp/modulo02/src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
 routes.get('/signup', UserController.create)
-routes.post('/signup', UserController.store)
+// avatar é o campo no banco de dados
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
