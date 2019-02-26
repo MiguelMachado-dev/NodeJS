1b0aa8c7d83e7cda10a79b435840ea07bbeafe50
Miguel Machado <miiguellmp@gmail.com>
2019-02-26 12:09:17 -0300

    hook para aguardar uma ação e executar criptografia de senha

--- Bootcamp/modulo03/src/app/models/User.js
+++ Bootcamp/modulo03/src/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   // mongoDB utiliza tipos de dados do proprio JavaScript
@@ -20,6 +21,18 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+})
+
+// Hook seja disparado antes de qualquer save no usuario
+// save é utilizado tanto para criação quanto para update
+UserSchema.pre('save', async function (next) {
+  // se a senha não foi modificada nessa alteração
+  // não faremos nada
+  if (!this.isModified('password')) {
+    return next()
+  }
+  // caso a senha seja modificada, faremos uma criptografia nela
+  this.password = await bcrypt.hash(this.password, 8)
 })
 
 module.exports = mongoose.model('User', UserSchema)
