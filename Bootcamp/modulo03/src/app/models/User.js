ac96c54cb511e3c9e3fc01b98f4fb86134314677
Miguel Machado <miiguellmp@gmail.com>
2019-02-26 11:42:34 -0300

    schema de user criado

---
+++ Bootcamp/modulo03/src/app/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  // mongoDB utiliza tipos de dados do proprio JavaScript
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
