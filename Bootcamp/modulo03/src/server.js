995a633cf3aaf98bb39718aa5d1fd811fa96b929
Miguel Machado <miiguellmp@gmail.com>
2019-02-26 11:36:13 -0300

    configuração mongoose

--- Bootcamp/modulo03/src/server.js
+++ Bootcamp/modulo03/src/server.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDev = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.middlewares()
     this.routes()
     // sem a views() por que não utilizaremos o nunjucks
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
   }
 
   middlewares () {
