f35d72a9bbb73b41c90dc6f3558ea0477bf54dcc
Miguel Machado <miiguellmp@gmail.com>
2019-02-27 10:42:21 -0300

    CRUD Ads

--- Bootcamp/modulo03/src/routes.js
+++ Bootcamp/modulo03/src/routes.js
@@ -4,11 +4,25 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
+// Chamando todos os Controllers sem precisar chamar um a um
 const controllers = require('./app/controllers')
 
 routes.post('/users', controllers.UserController.store)
 routes.post('/sessions', controllers.SessionController.store)
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
+// Todas as rotas daqui para baixo esteja configurada para não aceitar se o user
+// não estiver autenticado
+routes.use(authMiddleware)
+
+/**
+ * ADS
+ */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+// .put utiliza quando queremos atualizar alguma coisa
+routes.put('/ads/:id', controllers.AdController.update)
+// .delete para quando queremos deletar alguma coisa ¯\_(ツ)_/¯
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
