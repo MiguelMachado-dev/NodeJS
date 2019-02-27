faee09a0476d0fbec753e2decca911fece2bff65
Miguel Machado <miiguellmp@gmail.com>
2019-02-27 17:57:58 -0300

    adicionando validação nas rotas

--- Bootcamp/modulo03/src/routes.js
+++ Bootcamp/modulo03/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const validate = require('express-validation')
 
 const routes = express.Router()
 
@@ -6,9 +7,18 @@ const authMiddleware = require('./app/middlewares/auth')
 
 // Chamando todos os Controllers sem precisar chamar um a um
 const controllers = require('./app/controllers')
+const validators = require('./app/validators')
 
-routes.post('/users', controllers.UserController.store)
-routes.post('/sessions', controllers.SessionController.store)
+routes.post(
+  '/users',
+  validate(validators.User),
+  controllers.UserController.store
+)
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  controllers.SessionController.store
+)
 
 // Todas as rotas daqui para baixo esteja configurada para não aceitar se o user
 // não estiver autenticado
@@ -19,14 +29,18 @@ routes.use(authMiddleware)
  */
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
-routes.post('/ads', controllers.AdController.store)
+routes.post('/ads', validate(validators.Ad), controllers.AdController.store)
 // .put utiliza quando queremos atualizar alguma coisa
-routes.put('/ads/:id', controllers.AdController.update)
+routes.put('/ads/:id', validate(validators.Ad), controllers.AdController.update)
 // .delete para quando queremos deletar alguma coisa ¯\_(ツ)_/¯
 routes.delete('/ads/:id', controllers.AdController.destroy)
 
 /**
  * PURCHASES
  */
-routes.post('/purchases', controllers.PurchaseController.store)
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  controllers.PurchaseController.store
+)
 module.exports = routes
