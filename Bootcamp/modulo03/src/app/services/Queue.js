d6c04062008d529af2532c9e9e218976b9ef9178
Miguel Machado <miiguellmp@gmail.com>
2019-02-28 10:37:47 -0300

    chamando sentry na queue

--- Bootcamp/modulo03/src/app/services/Queue.js
+++ Bootcamp/modulo03/src/app/services/Queue.js
@@ -1,4 +1,5 @@
 const kue = require('kue')
+const Sentry = require('@sentry/node')
 const redisConfig = require('../../config/redis')
 const jobs = require('../jobs')
 
@@ -6,5 +7,8 @@ const Queue = kue.createQueue({ redis: redisConfig })
 
 // Toda vez que um job for disparado com esta key, disparará o método handle
 Queue.process(jobs.PurchaseMail.key, jobs.PurchaseMail.handle)
+
+// Sempre que tiver um erro dentro de nossa fila, o sentry também vai capturar
+Queue.on('error', Sentry.captureException)
 
 module.exports = Queue
