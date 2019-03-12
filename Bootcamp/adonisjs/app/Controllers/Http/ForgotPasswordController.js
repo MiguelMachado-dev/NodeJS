af61359a66258d9c2962cc7b6c570fc8a5d392c9
Miguel Machado <miiguellmp@gmail.com>
2019-03-12 12:04:08 -0300

    enviando email

--- Bootcamp/adonisjs/app/Controllers/Http/ForgotPasswordController.js
+++ Bootcamp/adonisjs/app/Controllers/Http/ForgotPasswordController.js
@@ -13,6 +13,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await MediaList.send(
+        ['emails.forgot_password', 'emails.forgot_password-text'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('miguel.machado@gmail.com', 'Miguel Machado')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({
         error: {
