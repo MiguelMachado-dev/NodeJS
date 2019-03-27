17b8b947b436467e56b58ba40d7f8f45f490ce63
Miguel Machado <miiguellmp@gmail.com>
2019-03-26 22:19:50 -0300

    visualizaçao de arquivos

--- Bootcamp/adonisjs/app/Models/File.js
+++ Bootcamp/adonisjs/app/Models/File.js
@@ -2,8 +2,18 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Env = use('Env')
 
 class File extends Model {
+  static get computed () {
+    return ['url']
+  }
+
+  getUrl ({ id }) {
+    // Retorna parametro url no envio do arquivo
+    // entao é só passar esse parametro no img src no frontend
+    return `${Env.get('APP_URL')}/files/${id}`
+  }
 }
 
 module.exports = File
