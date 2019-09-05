f3ad14504cda676fd1535303d66d8cc4596951ab
Miguel Machado <miiguellmp@gmail.com>
2019-09-05 08:52:44 -0300

    just logging port

--- NewBootcamp/GoBarber/src/server.js
+++ NewBootcamp/GoBarber/src/server.js
@@ -2,3 +2,7 @@ import app from './app';
 
 // * App PORT
 app.listen(3334);
+// eslint-disable-next-line no-console
+console.info(
+  '----------------------------------\nServidor escutando porta 3334\n----------------------------------'
+);
